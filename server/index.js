b2d664525a7dabb365c19dc502688d5439ad6b3b
Kimutaieddy <eddykimutai@outlook.com>
2024-12-09 09:59:21 +0300

    Add CORS support and update server listening port to 8080

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express(); 
+const cors = require('cors');
+
+app.use(cors());
 
 
 app.get('/', (req, res) => {  
     res.send('Hello from our server');
+}
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 }
