74f1bb61b7ec64a34c2aaf52a5e8218da4bd4ab6
Kimutaieddy <eddykimutai@outlook.com>
2024-12-09 10:57:18 +0300

    Fix syntax errors and improve server listening logic

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const app = express(); 
+onst express = require('express');
+const app = express();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors()) ;
 
+app.get('/', (req, res) => {
+      res.send('Hello from our server!')
+});
 
-app.get('/', (req, res) => {  
-    res.send('Hello from our server');
-}
-
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-}
+app.listen(8080,'0.0.0.0' () => {
+      console.log('server listening on port 8080')
+}) ;
